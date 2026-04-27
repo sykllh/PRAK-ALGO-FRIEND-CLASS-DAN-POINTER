7f172409c1c834b67b56cde684e0ce1eaaf595bd
sykllh <sykllhidmi@gmail.com>
2026-04-27 15:37:15 +0700

    membuat program utamnya

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -6,4 +6,13 @@ class mahasiswa
 private:
     string nama;
 public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
 }
+
+int main
